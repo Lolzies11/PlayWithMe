5727048016499ed7c7318d985e160819ff37cc3f
naiyume <naiyume>
2020-05-16 17:08:54 -0400

    Updates

---
+++ precache-manifest.174be466f35453a9d945b0200c0d3b66.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85415eeca9582497e72f5e40f6f0af21",
+    "url": "/PlayWithMe/index.html"
+  },
+  {
+    "revision": "06211405c6d68a5ebbda",
+    "url": "/PlayWithMe/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "17c79084c1e20441ba34",
+    "url": "/PlayWithMe/static/js/2.a489c2db.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/PlayWithMe/static/js/2.a489c2db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06211405c6d68a5ebbda",
+    "url": "/PlayWithMe/static/js/main.bdb6ef9b.chunk.js"
+  },
+  {
+    "revision": "3020bc9ccb1f460d9ed2",
+    "url": "/PlayWithMe/static/js/runtime-main.859bcbb7.js"
+  },
+  {
+    "revision": "7508b5323ad17b5befbbb51e9f2e64c5",
+    "url": "/PlayWithMe/static/media/abc.7508b532.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/PlayWithMe/static/media/logo.5d5d9eef.svg"
+  }
+]);
